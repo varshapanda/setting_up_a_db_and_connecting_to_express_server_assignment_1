6cbab902dae4ac2967fa7e8a104ad422fabdb14d
varshapanda <varsha.vijayashwini@kalvium.community>
2025-01-03 06:12:12 +0530

    completed-Setting up a DB and connecting to Express server - Practice assignment #1

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const { resolve } = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
 
 const app = express();
-const port = 3010;
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>{
+  console.log('Connected to Database');
+})
+.catch((error)=>{
+  console.error(`Error connecting to database: ${error.message}`)
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.get('/',(req,res)=>{
+  res.send('<h3>Backend for Customer Management System is running</h3>')
+})
+
+const PORT = process.env.PORT||3000;
+app.listen(PORT,()=>{
+  console.log(`Your server is running on PORT http://localhost:${PORT}`);
+})
